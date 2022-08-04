954db181607f412bf1bae6898d87bb0d4ce9ca0e
Chloe Lee <chloelee00516@gmail.com>
2022-08-04 19:20:32 +0900

    .
    .

--- app.js
+++ app.js
@@ -1,21 +1,97 @@
 "use strict";
+const colorOptions = Array.from(document.getElementsByClassName("color-option"));
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const lineWidth = document.getElementById("line-width");
+const color = document.getElementById("color"); 
+const modeBtn = document.getElementById("mode-btn");
+const resetBtn = document.getElementById("reset-btn");
+const eraserBtn = document.getElementById("eraser-btn");
+
+const CANVAS_WIDTH = 500;
+const CANVAS_HEIGHT = 500;
+
 //tell js the size of the canvas
-canvas.height = 500;
-canvas.width = 500;
+canvas.height = CANVAS_HEIGHT;
+canvas.width = CANVAS_WIDTH;
 
-ctx.rect(100,100, 50, 50);
-ctx.rect(150,150, 50, 50);
-ctx.fill();
+ctx.lineWidth = lineWidth.value;
+let isPainting = false;
+let isFilling = false;
 
-ctx.beginPath();
-ctx.rect(200,200, 50, 50);
-ctx.rect(250,250, 50, 50);
-ctx.fillStyle = "red";
-ctx.fill();
+function onMove(e) {
+    if (isPainting) {
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.beginPath();
+    ctx.moveTo(e.offsetX, e.offsetY);
+   
+}
 
-ctx.beginPath();
-ctx.rect(300,300, 50, 50);
-ctx.fillStyle = "yellow";
-ctx.fill();
+
+
+function onMouseDown() {
+    isPainting = true;
+}
+
+function cancelPainting() {
+    isPainting = false;
+    ctx.beginPath();
+}
+function onLineWidthChange(e) {
+
+   ctx.lineWidth = e.target.value; 
+}
+
+function onColorChange(e) {
+    ctx.fillStyle = e.target.value;
+    ctx.strokeStyle = e.target.value;
+}
+
+function onColorClick(e) {
+    const colorValue = e.target.dataset.color;
+    ctx.fillStyle = colorValue;
+    ctx.strokeStyle = colorValue;
+    color.value = colorValue;
+}
+function onCanvasClick() {
+    if (isFilling) {
+        ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+    }
+    
+}
+
+function onModeClick() {
+    if (isFilling) {
+        isFilling = false;
+        modeBtn.innerText = "Fill";
+    } else {
+        isFilling = true;
+        modeBtn.innerText = "Draw";
+    }
+}
+
+function onResetClick() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+}
+
+function onEraserClick() {
+    ctx.strokeStyle = "white";
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+  
+}
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", onMouseDown);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+colorOptions.forEach(color => color.addEventListener("click", onColorClick));
+modeBtn.addEventListener("click", onModeClick);
+resetBtn.addEventListener("click", onResetClick);
+eraserBtn.addEventListener("click", onEraserClick);
